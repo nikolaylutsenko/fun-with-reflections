cd89bb77e0057d26939283926cae3f8034c0a20f
Mykola Lutsenko <Mykola.Lutsenko@dataart.com>
2022-08-09 18:31:47 +0300

    add fun implementation of GetAll method

---
+++ Repository.cs
@@ -0,0 +1,43 @@
+// this method receive as TResult only POCO class or class implemented generic IEnumerable<>
+public async Task<TResult?> GetAsync<TResult>(
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            params string[] includes) where TResult : class
+{
+    var query = _dbSet as IQueryable<TEntity>;
+
+    if (filter != null)
+    {
+        query = query.Where(filter);
+    }
+
+    if (includes.Any())
+    {
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+    }
+
+    if (orderBy != null)
+    {
+        query = orderBy(query);
+    }
+
+    // add just a bit of black magic
+    var innerType = typeof(TResult).GetGenericArguments().FirstOrDefault();
+    if (innerType is null) // if inner type is null it means that we provide POCO type
+    {
+        var result = (await query.FirstOrDefaultAsync()) as TResult;
+        return result;
+    } // else we provide generic type we must check if it derives from IEnumerable<>
+    else if (typeof(TResult).GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+    {
+        var result = (await query.ToListAsync()) as TResult;
+        return result;
+    }
+    else // if none of those, we don't deal with it
+    {
+        throw new Exception($"Not supported type {typeof(TResult).Name}");
+    }
+}
